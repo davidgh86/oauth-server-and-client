21123a2b46524e7fbd64d2bcaf67e422d52dd86d
david <jgarcia.ext@aenor.com>
2020-01-20 16:36:34 +0100

    oauth and resource server

---
+++ resourceServer/src/main/java/com/david/resourceServer/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.david.resourceServer;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+
+}
